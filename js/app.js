768c9df4f07d6f57784ee5fe795215261e7dc97f
Phoebe <phoebe.wilckens@gmail.com>
2022-02-22 18:45:17 -0300

    mapping data

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
+// mapeamos los productos
+fetch("./data.json", {
+  method: "GET",
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.table(data);
+    data.map((mug) => {
+      console.log(mug.img);
+      const divProductos = document.querySelector("#productos");
+      divProductos.innerHTML += `
+        <div class="containerProduct">
+            <img src="${mug.img}" class="imgProduct">
+            <h2>${mug.name}</h2>
+            <div class="infoProduct">
+                <h4>Stock: ${mug.stock}</h4>
+                <h3>$${mug.precio}</h3>
+            </div>
+            <div class="infoProduct">
+                <div class="centrar">
+                <button class="addProduct">Agregar</button>
+                </div>
+        </div>            `;
+    });
+  });
+
 const CalcularCosto = () => {
   const getBtn = document.getElementById("calcular");
   const getResult = document.getElementById("resultado");
@@ -8,13 +36,45 @@ const CalcularCosto = () => {
     const iva = 19;
     let precioConIva = ((precio1 + precio2) * iva) / 100;
     console.log(precioConIva, "nos sumamos?");
-    let precioTotal = Math.floor(precio1 + precio2 + precioConIva);
+    let precioTotal = new Intl.NumberFormat("es-CL").format(
+      Math.floor(precio1 + precio2 + precioConIva)
+    );
     if (precioTotal >= 20000) {
-      getResult.innerHTML = `El total de tu compra es: $ ${precioTotal}`;
+      getResult.innerHTML = `El total de tu compra es: $ ${precioTotal} CLP`;
     } else {
-      getResult.innerHTML = `El total de tu compra es: $ ${precioTotal}, necesitas agregar más productos a tu carrito.`;
+      getResult.innerHTML = `El total de tu compra es: $ ${precioTotal} CLP, necesitas agregar más productos a tu carrito.`;
     }
   });
 };
 
 CalcularCosto();
+
+//   const firstBatch = recetas.slice(0, 6);
+
+//   console.log(firstBatch, "corte");
+
+//   const [currentRecetasRender, setCurrentRecetasRender] = useState(firstBatch);
+
+//   const setCurrentRecetasRenderHandler = () => {
+//     do {
+//       setCurrentRecetasRender(
+//         recetas.slice(0, currentRecetasRender.length + 6)
+//       );
+//     } while (currentRecetasRender.length === recetas.length);
+// };
+
+//         <div className={handles.RecetaFooter}>
+//           {currentRecetasRender.length < 21 ? (
+//             <button
+//               className={handles.RecetaBtnSeeMore}
+//               onClick={setCurrentRecetasRenderHandler}
+//             >
+//               Ver más recetas
+//             </button>
+//           ) : (
+//             <p className={handles.RecetaNoMoreBlogs}>
+//               {" "}
+//               No hay más recetas por ahora
+//             </p>
+//           )}
+//         </div>;
